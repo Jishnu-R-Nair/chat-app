51442b03675334f05e04efc55ded0bab7dad1950
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-03-07 22:33:17 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const publicDirectory = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicDirectory));
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
