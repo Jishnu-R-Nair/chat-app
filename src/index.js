14a168675c240b9822f99b499727d6065cc370d3
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-03-08 10:42:06 +0530

    added socketio and its basic configurations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(publicDirectory));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New WebSocket connection');
+});
+
+server.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
