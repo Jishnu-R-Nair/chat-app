680d966504ecd841c633d0b8bce80550802f9f03
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-03-09 21:57:45 +0530

    added changes to share location, added ack for messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,45 @@
 const socket = io();
 
-const weatherForm = document.querySelector('#message-form');
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
 
-weatherForm.addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const message = e.target.elements.message.value;
-  socket.emit('sendMessage', message);
+  const message = $messageFormInput.value;
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
+  socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
+    if (error) return console.log(error);
+    console.log('Message got delivered');
+  });
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation)
+    return alert('Geolocation is not supported by your browser');
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      },
+      () => {
+        console.log('Location shared!');
+        $sendLocationButton.removeAttribute('disabled');
+      }
+    );
+  });
 });
 
 socket.on('message', (message) => {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, '../public');
@@ -9,6 +10,7 @@ const publicDirectory = path.join(__dirname, '../public');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const filter = new Filter();
 
 app.use(express.static(publicDirectory));
 
@@ -17,8 +19,19 @@ io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A user has joined');
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    if (filter.isProfane(message)) return callback('Profanity is not allowed');
+
     io.emit('message', message);
+    callback();
+  });
+
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+    io.emit(
+      'message',
+      `https://www.google.com/maps?q=${latitude},${longitude}`
+    );
+    callback();
   });
 
   socket.on('disconnect', () => {
